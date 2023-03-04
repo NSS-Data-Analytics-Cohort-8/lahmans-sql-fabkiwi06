00de09a8de43092391c15962e009e1be28e82ec0
Kieran Bailey <k.d.bailey88@gmail.com>
2023-03-04 13:47:54 -0600

    started 8 but dont feel good about it

--- script/baseball.sql
+++ script/baseball.sql
@@ -176,7 +176,14 @@ FROM cte
 LIMIT 1;
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+select * from homegames
+WHERE year = 2016
 
+SELECT park, team, attendance/games AS att_per_game
+FROM homegames
+WHERE year = 2016
+ORDER BY att_per_game DESC
+LIMIT 5
 
 
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
